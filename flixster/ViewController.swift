8ea2f4527e95deb3719479d76c1ee8b1a03bd1e7
Radhyyah Hossain <radiabxd@gmail.com>
2020-09-25 19:36:40 -0400

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Radhyyah Hossain on 9/25/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
