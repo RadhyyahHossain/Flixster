48ce4926c63578b013a059a54e5bf2c8d42d7e74
Radhyyah Hossain <radiabxd@gmail.com>
2020-09-25 21:34:22 -0400

    trying to get images, about to install cocoa pods

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -75,6 +75,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        
         return cell
     }
     
